a47e2e7047d8780cec3f9a6efeb9377ea5a7a9d5
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-05 21:37:27 +0300

    chore: install & configure prettier, lint-staged, eslint plugins & configs

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 120,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
